fceaf1962e174fbb5569ea4317334131c9f62b43
runfa <1062054504@qq.com>
2015-09-22 19:37:44 +0800

    a
    ab

---
+++ abs/a.cpp
@@ -0,0 +1,16 @@
+#include<string>
+#include<iostream>
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+int main()
+{
+
+
+string fruit;
+fruit="abcd";
+cout<<fruit<<endl;
+cin.get();
+return 0;
+}
