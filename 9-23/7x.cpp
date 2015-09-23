704abc479b5379cefd7a4d92a0865701c4847669
runfa <1062054504@qq.com>
2015-09-23 23:36:42 +0800

    Revert "9-23"
    This reverts commit 326c833c28d4b267ae719c053e70e8e4c3302128.

--- 9-23/7x.cpp
+++
@@ -1,9 +0,0 @@
-//编写并试运行一个程序，找出哪种数学运算符可能将变量的值由3个数值常量inf、-inf、和nan改变为其他的值
-//这个不知道要怎么弄
-#include<iostream>
-using namespace std;
-int main()
-{
-	cout<<"int= "<<inf<<endl
-		<<"-inf= "<<-inf<<endl
-		<<"nan= "<<nan<<endl;
